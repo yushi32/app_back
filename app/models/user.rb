c002104e401359c9f80accb3fc2b8e4bbf38df5f
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-05 03:34:41 +0900

    :sparkles:feat: Userモデルにバリデーションとユーザーインスタンスを返すメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name,  presence: true
+  validates :uid,   presence: true, uniqueness: true
+
+  def self.find_or_create_user(user_info)
+    user = User.find_by(uid: user_info[:uid])
+    return user if user
+
+    User.create!(uid: user_info[:uid], name: user_info[:name])
+  end
 end
