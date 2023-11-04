36195297a3e5fd20a946a3bff5062b32c6fd18c9
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-05 04:48:31 +0900

    :sparkles:feat: Bookmarkモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   validates :name,  presence: true
   validates :uid,   presence: true, uniqueness: true
 
+  has_many :bookmarks, dependent: :destroy
+
   def self.find_or_create_user(user_info)
     user = User.find_by(uid: user_info[:uid])
     return user if user
