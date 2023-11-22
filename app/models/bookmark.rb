44ac10a93d5d17b2bf4906f4630729762dbf41a9
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-23 03:13:47 +0900

    :bug:fix: destroyアクション実行時に中間テーブルのレコードを削除するように修正

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,7 +5,7 @@ class Bookmark < ApplicationRecord
   validates :caption, length: { maximum: 140 }
 
   belongs_to :user
-  has_many :bookmark_tags
+  has_many :bookmark_tags, dependent: :destroy
   has_many :tags, through: :bookmark_tags
 
   enum status: { unnotified:0, notified:1, read:2 }
