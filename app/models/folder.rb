1df73cc4482453fce2b7114a1540caec264db283
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-30 14:32:49 +0900

    :sparkles:feat: UserモデルとFolderモデル間のアソシエーションを定義

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,2 +1,5 @@
 class Folder < ApplicationRecord
+  belongs_to :user
+  belongs_to :parent, class_name: :Folder, optional: true
+  has_many :children, class_name: :Folder, foreign_key: :parent_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :uid,   presence: true, uniqueness: true
 
   has_many :bookmarks, dependent: :destroy
+  has_many :folders, dependent: :destroy
 
   def self.find_or_create_user(user_info)
     user = User.find_by(uid: user_info[:uid])
