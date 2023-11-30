3f4ad3018d9c9bc3cff72bca35c729b09335b809
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-30 14:33:12 +0900

    :sparkles:feat: Folderモデルにバリデーションを定義

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,6 @@
 class Folder < ApplicationRecord
+  validates :name, presence: true
+
   belongs_to :user
   belongs_to :parent, class_name: :Folder, optional: true
   has_many :children, class_name: :Folder, foreign_key: :parent_id
