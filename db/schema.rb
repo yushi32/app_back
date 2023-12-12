7a4aced03d1e25f6abf3df86eede2d3ae599f7da
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-12 12:11:03 +0900

    :sparkles:feat: foldersテーブルにpositionカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_060748) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_021218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_060748) do
     t.bigint "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "position", null: false
     t.index ["parent_id"], name: "index_folders_on_parent_id"
     t.index ["user_id"], name: "index_folders_on_user_id"
   end
