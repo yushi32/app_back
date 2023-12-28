3ff48f8062264de66607bcf541872df6cf20e9ff
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-28 15:58:07 +0900

    :sparkles:feat: Notificationモデルを作成

---
+++ app/models/notification.rb
@@ -0,0 +1,4 @@
+class Notification < ApplicationRecord
+  enum on_weekday: { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
+  enum mode: { default: 0, folder_only: 1, tag_only: 2 }
+end
