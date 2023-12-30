ffd2fc0a9b2e0da7edeea22f88b5bc2b04704408
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-31 00:16:35 +0900

    :sparkles:feat: Notificationsコントローラーを作成し、createアクションでレコードを作成した時にデフォルト値を設定する

---
+++ app/controllers/api/v1/notifications_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::NotificationsController < Api::V1::BaseController
+  def create
+    notification = current_user.build_notification(default_setting)
+    if notification.save
+      head :no_content
+    else
+      render json: { errors: notification.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def default_setting
+    now = Time.zone.now
+    {
+      start_date: now,
+      scheduled_time: Time.zone.local(now.year, now.month, now.day, 20),
+      on_weekday: :saturday
+    }
+  end
+end
