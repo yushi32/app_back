c62a15ce51c1e83948401354e5a7b4f22bc0d49a
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-31 00:17:57 +0900

    :sparkles:feat: 次の通知の実行日時を計算するnext_run_atメソッドを作成

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -17,6 +17,26 @@ class Notification < ApplicationRecord
     errors.add(:base, 'モードとカテゴリータイプが一致していません。')
   end
 
+  # 次の通知の実行日時を計算する
+  def next_run_at
+    now = Time.zone.now
+    # 指定された時刻でDateTimeを作成する
+    target_datetime = scheduled_time.change(
+      year: now.year, 
+      month: now.month, 
+      day: now.day
+    )
+
+    # 次に来る指定された曜日の日付を計算する
+    days_until_target = ((on_weekday_before_type_cast - now.wday) % 7).days
+    target_datetime = target_datetime + days_until_target
+
+    # もし計算された日時が過去になる場合は、次の週の同じ曜日に変更する
+    target_datetime += 7.days if target_datetime.past?
+
+    target_datetime
+  end
+
   private
 
   def default_mode?
