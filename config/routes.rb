86133a8ac214def0cf95da9ae4f7326c2ce83bdd
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-31 16:14:32 +0900

    :adhesive_bandage:fix: タイポ修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   Sidekiq::Web.use(Rack::Auth::Basic) do |user_id, password|
-    [user_id, password] == ['ENV['SIDEKIQ_USER_ID']', ENV['SIDEKIQ_USER_PASSWORD']]
+    [user_id, password] == [ENV['SIDEKIQ_USER_ID'], ENV['SIDEKIQ_USER_PASSWORD']]
   end
   mount Sidekiq::Web, at: '/sidekiq'
 
