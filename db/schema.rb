fc74e3c4727385da4fccb11b7461db48eb4e09f8
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-01-05 19:00:30 +0900

    :card_file_box:feat: notificationsテーブルにjob_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_014544) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_05_095419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_014544) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "job_id"
     t.index ["category_type", "category_id"], name: "index_notifications_on_category"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
