ca6ecb3a56665c5bf279308bb9785922921942be
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-01-18 23:26:53 +0900

    :recycle:refactor: 冗長性を低下させるためにLineApiClientで使用する定数LINE_CHANNEL_IDをCHANNEL_IDに変更

--- app/lib/line_api_client.rb
+++ app/lib/line_api_client.rb
@@ -2,15 +2,15 @@ require 'net/http'
 require 'uri'
 
 module LineApiClient
-  LINE_CHANNEL_ID = ENV["LINE_CHANNEL_ID"].freeze
+  CHANNEL_ID = ENV['LINE_CHANNEL_ID'].freeze
   CLIENT_SECRET = ENV['LINE_CHANNEL_SECRET'].freeze
   CHANNEL_ACCESS_TOKEN = ENV['LINE_CHANNEL_ACCESS_TOKEN'].freeze
 
   def get_line_user_id(id_token)
     uri = URI.parse('https://api.line.me/oauth2/v2.1/verify')
     res = Net::HTTP.post_form(uri, {
-      'id_token' => id_token,
-      'client_id' => LINE_CHANNEL_ID
+      'id_token': id_token,
+      'client_id': CHANNEL_ID
     })
     if res.is_a?(Net::HTTPSuccess)
       JSON.parse(res.body)['sub']
