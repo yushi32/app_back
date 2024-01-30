3e2cd28e9c945384b5067a135955a2193a80500e
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-01-30 17:01:07 +0900

    :card_file_box:add: Tagモデルのname属性にユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_28_175355) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_30_072456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_28_175355) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "user_tags", force: :cascade do |t|
