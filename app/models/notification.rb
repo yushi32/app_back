b9e51e8b77a863a14ec0ad0bdb213b8a4444d733
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-02-13 02:01:59 +0900

    :recycle:refactor: Metrics/AbcSizeの違反を修正するためにNotificationモデルのnext_run_atメソッドをリファクタリング

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -22,21 +22,14 @@ class Notification < ApplicationRecord
   def next_run_at
     now = Time.zone.now
     # 指定された時刻でDateTimeを作成する
-    target_datetime = scheduled_time.change(
-      year: now.year,
-      month: now.month,
-      day: now.day
-    )
+    target_datetime = create_target_datetime(now)
 
     # 通知設定に従って次の実行日時を計算する
     # 計算した日時が過去になってしまった場合は設定に従って先送りする
     if on_weekday.present?
-      days_until_target = ((on_weekday_before_type_cast - now.wday) % 7).days
-      target_datetime += days_until_target
-      target_datetime += 7.days while target_datetime.past?
+      target_datetime = calculate_notification_date_for_weekday(target_datetime)
     elsif interval_days.present?
-      target_datetime += interval_days.days
-      target_datetime += interval_days.days while target_datetime.past?
+      target_datetime = calculate_notification_date_by_interval(target_datetime)
     end
 
     target_datetime
@@ -56,5 +49,26 @@ class Notification < ApplicationRecord
   # tagモードが正しいかどうかを判定
   def tag_mode?
     tag_only? && category_type == 'Tag'
+  end
+
+  def create_target_datetime(now)
+    scheduled_time.change(
+      year: now.year,
+      month: now.month,
+      day: now.day
+    )
+  end
+
+  def calculate_notification_date_for_weekday(target_datetime)
+    days_until_target = ((on_weekday_before_type_cast - target_datetime.wday) % 7).days
+    target_datetime += days_until_target
+    target_datetime += 7.days while target_datetime.past?
+    target_datetime
+  end
+
+  def calculate_notification_date_by_interval(target_datetime)
+    target_datetime += interval_days.days
+    target_datetime += interval_days.days while target_datetime.past?
+    target_datetime
   end
 end
