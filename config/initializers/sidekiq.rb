ce376eaab090239ea55bc073fcae43a21e155c8d
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-02-15 23:08:02 +0900

    :wrench:chore: ハートビートの間隔を50秒に変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
   Sidekiq::BasicFetch::TIMEOUT = 60
+  Sidekiq::Launcher.send(:remove_const, 'BEAT_PAUSE')
+  Sidekiq::Launcher.const_set('BEAT_PAUSE', 50)
 end
 
 Sidekiq.configure_client do |config|
